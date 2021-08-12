5668032ec834e8c8da5e9443b5cfae6ba02cb974
FAMASoon <famasoon@gmail.com>
2021-08-13 01:00:34 +0900

    he-

---
+++ leetcode/7.reverse-integer.go
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=7 lang=golang
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+import "strconv"
+func reverse(x int) int {
+  
+}
+// @lc code=end
+
---
+++ leetcode/go.mod
@@ -0,0 +1,3 @@
+module github.com/famasoon/kyopro/leetcode
+
+go 1.16
